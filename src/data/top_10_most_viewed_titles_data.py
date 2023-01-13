96d2ad1ad28c0f4b39e3a11451fe4a2c080ef932
Berkay Kush <berkayilhankush@gmail.com>
2023-01-13 10:03:43 +0100

    Fixed the stacking issue for the top 10 most watched titles graph

--- src/data/top_10_most_viewed_titles_data.py
+++ src/data/top_10_most_viewed_titles_data.py
@@ -1,9 +1,14 @@
 def create_top_10_most_viewed_titles_data(viewing_activity_data):
-    return (
-        viewing_activity_data.groupby(["Title", "Year"], as_index=False)[
-            "Duration in Hours"
-        ]
+    top_10_viewed_titles = (
+        viewing_activity_data.groupby(["Title"], as_index=False)["Duration in Hours"]
         .sum()
         .sort_values("Duration in Hours", ascending=False)
         .head(10)
-    )
+    )["Title"]
+
+    top_10_most_viewed_titles_over_years_data = viewing_activity_data.groupby(
+        ["Title", "Year"], as_index=False
+    )["Duration in Hours"].sum()
+
+    mask = top_10_most_viewed_titles_over_years_data["Title"].isin(top_10_viewed_titles)
+    return top_10_most_viewed_titles_over_years_data.loc[mask]
