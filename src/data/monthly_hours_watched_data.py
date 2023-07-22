116f2aa7301fe93d45c6c8c2080977e8258ae023
Berkay Kush <berkay.i.kush@gmail.com>
2023-07-22 11:20:14 +0300

    Show the months with 0 total hours viewed in the years before the current one

--- src/data/monthly_hours_watched_data.py
+++ src/data/monthly_hours_watched_data.py
@@ -1,12 +1,23 @@
+import pandas as pd
+
+
 def create_monthly_hours_watched_data(viewing_activity_data):
     monthly_hours_watched_data = viewing_activity_data.copy()
     monthly_hours_watched_data["Month"] = (
         monthly_hours_watched_data["Start Time"].sort_values().dt.month
     )
 
-    monthly_hours_watched_data = monthly_hours_watched_data.groupby(
-        ["Month", "Year"], as_index=False
-    )["Duration in Hours"].sum()
+    monthly_hours_watched_data = (
+        monthly_hours_watched_data.groupby(["Month", "Year"], as_index=False)[
+            "Duration in Hours"
+        ]
+        .sum()
+        .sort_values("Year")
+    )
+
+    monthly_hours_watched_data = add_missing_months_for_years_before_curr(
+        monthly_hours_watched_data
+    )
 
     # Replace the month numbers with their names
     monthly_hours_watched_data["Month"] = monthly_hours_watched_data["Month"].replace(
@@ -27,3 +38,23 @@ def create_monthly_hours_watched_data(viewing_activity_data):
     )
 
     return monthly_hours_watched_data
+
+
+def add_missing_months_for_years_before_curr(monthly_hours_watched_data):
+    filtered_years = monthly_hours_watched_data["Year"].unique()[
+        : len(monthly_hours_watched_data["Year"].unique()) - 1
+    ]
+
+    for year in filtered_years:
+        for month in range(1, 13):
+            if not monthly_hours_watched_data[
+                (monthly_hours_watched_data["Year"] == year)
+                & (monthly_hours_watched_data["Month"] == month)
+            ].any()["Month"]:
+                new_row = {"Month": month, "Year": year, "Duration in Hours": 0}
+                monthly_hours_watched_data = pd.concat(
+                    [monthly_hours_watched_data, pd.DataFrame([new_row])],
+                    ignore_index=True,
+                )
+
+    return monthly_hours_watched_data.sort_values(["Month", "Year"])
