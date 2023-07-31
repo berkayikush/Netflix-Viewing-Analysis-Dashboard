a17cf94793ed5d6728ed106bee6f079ea19ee644
Berkay Kush <berkay.i.kush@gmail.com>
2023-07-31 17:50:23 +0300

    Remove the print statment inside create_most_viewed_titles_data()

--- src/data/most_viewed_titles_data.py
+++ src/data/most_viewed_titles_data.py
@@ -8,11 +8,6 @@ def create_most_viewed_titles_data(viewing_activity_data):
         most_viewed_titles_over_years_data["Duration in Hours"] > 0.5
     ]
 
-    print(
-        most_viewed_titles_over_years_data.sort_values(
-            "Duration in Hours", ascending=False
-        )
-    )
     return most_viewed_titles_over_years_data.sort_values(
         "Duration in Hours", ascending=False
     )
